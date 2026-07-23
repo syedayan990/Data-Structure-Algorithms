a93b9cc7d6373501f7163a99a36e571c01ae6bbe
syedayan990 <ayanhusain903@gmail.com>
2026-07-24 03:58:54 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.39%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -21,15 +21,35 @@ public:
         //     swap(nums[i] , nums[smallestIndex]);
         // }
 
+        // int n = nums.size();
+        // for(int i=1; i<n; i++){
+        //     int curr = nums[i];
+        //     int prev = i-1;
+        //     while(prev >= 0 && nums[prev] > curr){
+        //         nums[prev + 1] = nums[prev];
+        //         prev--;
+        //     }
+        //     nums[prev + 1] = curr;
+        // }
+
         int n = nums.size();
-        for(int i=1; i<n; i++){
-            int curr = nums[i];
-            int prev = i-1;
-            while(prev >= 0 && nums[prev] > curr){
-                nums[prev + 1] = nums[prev];
-                prev--;
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+             swap(nums[low] , nums[mid]);
+             low++;
+             mid++;
             }
-            nums[prev + 1] = curr;
+            else if(nums[mid] == 1){
+               mid++;
+            }
+            else{
+                  swap(nums[high] , nums[mid]);
+                  high--;
+            }
         }
     }
 };
