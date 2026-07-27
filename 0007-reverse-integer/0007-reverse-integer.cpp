6595d03ccffbd8bbac49ccffe438e7c52fa43f5c
Syed Ayan Husain <ayanhusain903@gmail.com>
2026-07-28 02:02:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (54.79%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int n) {
+        int revNum = 0;
+        
+        while(n != 0){
+            int dig = n % 10;
+            if(revNum > INT_MAX/10 || revNum < INT_MIN/10){
+                return 0;
+            }
+            revNum = revNum * 10 + dig;
+            n = n/10;
+        }
+        return revNum;
+    }
+};
