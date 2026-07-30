bea982840dfb5ef8430213b7e2ef23982c14d11e
Syed Ayan Husain <ayanhusain903@gmail.com>
2026-07-31 04:58:08 +0530

    Time: 0 ms (100%), Space: 31.3 MB (82.38%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
+
+    int binarySearch(vector<int>& nums, int target , int st , int end){
+       if(st <= end){
+       int mid = st + (end-st)/2;
+
+       if(nums[mid] == target){
+        return mid;
+       } 
+       else if(nums[mid] <= target){
+        return binarySearch(nums , target , mid + 1 , end);
+       }else{
+        return binarySearch(nums , target , st , mid - 1);
+       }
+
+    }
+    return -1;
+    }
+
     int search(vector<int>& nums, int target) {
-        int mid;
-        int start=0;
-        int n = nums.size();
-        int end = n-1;
-        while(start <= end){
-          mid = start + (end-start)/2;
-            if(target > nums[mid]){
-                start= mid+1;
-            }
-            else if(target < nums[mid]){
-                end=mid-1;
-            }
-            else{
-                return mid;
+        // by binary search
+        // int mid;
+        // int start=0;
+        // int n = nums.size();
+        // int end = n-1;
+        // while(start <= end){
+        //   mid = start + (end-start)/2;
+        //     if(target > nums[mid]){
+        //         start= mid+1;
+        //     }
+        //     else if(target < nums[mid]){
+        //         end=mid-1;
+        //     }
+        //     else{
+        //         return mid;
 
-            }
+        //     }
 
-        }
-        return -1;
+        // }
+        // return -1;
 
+
+
+
+    //    by recursive binary search--
+    return binarySearch(nums,  target , 0 , nums.size()-1);
+   
     }
 };
