b34ef4708fc96ca521b79b5ce8916646154092ed
Syed Ayan Husain <ayanhusain903@gmail.com>
2026-08-05 17:44:52 +0530

    Time: 0 ms (100%), Space: 19.4 MB (63.48%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,16 +11,43 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1 == NULL || list2 == NULL){
-            return list1 == NULL ? list2 : list1;
+        // if(list1 == NULL || list2 == NULL){
+        //     return list1 == NULL ? list2 : list1;
+        // }
+
+        // if(list1->val <= list2->val){
+        //     list1->next = mergeTwoLists(list1->next , list2);
+        //     return list1;
+        // }else{
+        //     list2->next = mergeTwoLists(list1 , list2->next);
+        //     return list2;
+        // }
+
+        ListNode dummy(-1);
+        ListNode* temp = &dummy;
+
+        while(list1 != NULL && list2 != NULL)
+        {
+            if(list1->val <= list2->val)
+            {
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            else
+            {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+
+            temp = temp->next;
         }
 
-        if(list1->val <= list2->val){
-            list1->next = mergeTwoLists(list1->next , list2);
-            return list1;
-        }else{
-            list2->next = mergeTwoLists(list1 , list2->next);
-            return list2;
-        }
+        if(list1 != NULL)
+            temp->next = list1;
+
+        if(list2 != NULL)
+            temp->next = list2;
+
+        return dummy.next;
     }
 };
