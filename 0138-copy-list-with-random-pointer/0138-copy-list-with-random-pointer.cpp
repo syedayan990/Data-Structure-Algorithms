6db0cef5999dac345d6ab72ef7c7ea1419df7239
Syed Ayan Husain <ayanhusain903@gmail.com>
2026-08-05 19:20:57 +0530

    Time: 8 ms (27.57%), Space: 15 MB (71.3%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,36 +17,85 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if (head == NULL) {
+        // if (head == NULL) {
+        //     return NULL;
+        // }
+
+        // unordered_map<Node*, Node*> m;
+
+        // Node* newHead = new Node(head->val);
+        // Node* oldTemp = head->next;
+        // Node* newTemp = newHead;
+        // m[head] = newHead;
+
+        // while (oldTemp != NULL) {
+        //     Node* copyNode = new Node(oldTemp->val);
+        //     m[oldTemp] = copyNode;
+        //     newTemp->next = copyNode;
+
+        //     oldTemp = oldTemp->next;
+        //     newTemp = newTemp->next;
+        // }
+
+        // oldTemp = head;
+        // newTemp = newHead;
+
+        // while (oldTemp != NULL) {
+        //     newTemp->random =
+        //         m[oldTemp->random]; // oldtemp ka random pointer point to be
+        //                             // newTemp ka random ptr
+        //     oldTemp = oldTemp->next;
+        //     newTemp = newTemp->next;
+        // }
+        // return newHead;
+
+          if(head == NULL)
             return NULL;
+
+        Node* curr = head;
+
+        // Step 1: Insert copied nodes
+        while(curr != NULL)
+        {
+            Node* copy = new Node(curr->val);
+
+            copy->next = curr->next;
+            curr->next = copy;
+
+            curr = copy->next;
         }
 
-        unordered_map<Node*, Node*> m;
+        // Step 2: Set random pointers
+        curr = head;
 
-        Node* newHead = new Node(head->val);
-        Node* oldTemp = head->next;
-        Node* newTemp = newHead;
-        m[head] = newHead;
+        while(curr != NULL)
+        {
+            if(curr->random != NULL)
+            {
+                curr->next->random = curr->random->next;
+            }
 
-        while (oldTemp != NULL) {
-            Node* copyNode = new Node(oldTemp->val);
-            m[oldTemp] = copyNode;
-            newTemp->next = copyNode;
-
-            oldTemp = oldTemp->next;
-            newTemp = newTemp->next;
+            curr = curr->next->next;
         }
 
-        oldTemp = head;
-        newTemp = newHead;
+        // Step 3: Separate the lists
+        Node* dummy = new Node(-1);
+        Node* copyTail = dummy;
 
-        while (oldTemp != NULL) {
-            newTemp->random =
-                m[oldTemp->random]; // oldtemp ka random pointer point to be
-                                    // newTemp ka random ptr
-            oldTemp = oldTemp->next;
-            newTemp = newTemp->next;
+        curr = head;
+
+        while(curr != NULL)
+        {
+            Node* copy = curr->next;
+
+            curr->next = copy->next;
+
+            copyTail->next = copy;
+            copyTail = copy;
+
+            curr = curr->next;
         }
-        return newHead;
+
+        return dummy->next;
     }
 };
