bc57f4b9180d36872412480471f705dcff2e3350
Syed Ayan Husain <ayanhusain903@gmail.com>
2026-08-07 16:41:04 +0530

    Time: 3 ms (63.19%), Space: 11.8 MB (45.81%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -12,44 +12,35 @@ public:
 class Solution {
 public:
     Node* flatten(Node* head) {
-          if (head == nullptr)
-            return nullptr;
+     if(head == NULL){
+        return head;
+     }
+      
+      Node* curr = head;
+      while(curr != NULL){
+        if(curr->child != NULL){
+            //   flatten the child node
+            Node* next = curr->next;
+            curr->next = flatten(curr->child);
 
-        stack<Node*> st;
-        Node* curr = head;
+            curr->next->prev = curr;
 
-        while (curr != nullptr) {
+            curr->child = NULL;
 
-            // Agar child exist karta hai
-            if (curr->child != nullptr) {
-
-                // Current ke next ko stack me save karo
-                if (curr->next != nullptr) {
-                    st.push(curr->next);
-                }
-
-                // Child ko next bana do
-                curr->next = curr->child;
-                curr->child->prev = curr;
-
-                // Child pointer ko null kar do
-                curr->child = nullptr;
+            // find tail
+            while(curr->next != NULL){
+                curr = curr->next;
             }
 
-            // Agar current ka next nahi hai aur stack me node bachi hai
-            if (curr->next == nullptr && !st.empty()) {
-
-                Node* nextNode = st.top();
-                st.pop();
-
-                curr->next = nextNode;
-                nextNode->prev = curr;
+            // attach the tail with next ptr
+            if(next != NULL){
+                curr->next = next;
+                next->prev = curr;
             }
-
-            // Next node par move karo
-            curr = curr->next;
         }
 
-        return head;
+        curr = curr->next;
+      }
+      return head;
     }
 };
