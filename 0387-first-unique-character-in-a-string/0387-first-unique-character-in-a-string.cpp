fed50e16cc77718405d9d3c42901e4c3aa693548
Syed Ayan Husain <ayanhusain903@gmail.com>
2026-08-10 05:22:58 +0530

    Time: 20 ms (19.22%), Space: 15 MB (7.47%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -2,19 +2,36 @@ class Solution {
 public:
     int firstUniqChar(string s) {
 
-        int freq[26] = {0};
+        // int freq[26] = {0};
 
-        for (int i = 0; i < s.size(); i++) {
-            freq[s[i] - 'a']++;
+        // for (int i = 0; i < s.size(); i++) {
+        //     freq[s[i] - 'a']++;
+        // }
+
+        // for (int i = 0; i < s.size(); i++) {
+
+        //     if (freq[s[i] - 'a'] == 1) {
+        //         return i;
+        //     }
+        // }
+
+        // return -1;
+
+
+        unordered_map<char , int> m;
+        queue<int> Q;
+
+        for(int i=0; i<s.size(); i++){
+            if(m.find(s[i]) == m.end()){
+                Q.push(i);
+                }
+                m[s[i]]++;
+
+                while(Q.size() > 0 && m[s[Q.front()]] > 1){
+                    Q.pop();
+                } 
+
         }
-
-        for (int i = 0; i < s.size(); i++) {
-
-            if (freq[s[i] - 'a'] == 1) {
-                return i;
-            }
-        }
-
-        return -1;
+        return Q.empty() ? -1 : Q.front();
     }
 };
