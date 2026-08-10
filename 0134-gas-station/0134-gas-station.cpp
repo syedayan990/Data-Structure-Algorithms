e2d668074087a3481f25c4a1ebd8c3e8be2b2b81
Syed Ayan Husain <ayanhusain903@gmail.com>
2026-08-11 00:36:08 +0530

    Time: 0 ms (100%), Space: 112.6 MB (8.63%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        // int TotalGas = 0;
+        // int TotalCost = 0;
+
+        // for (int val : gas) {
+        //     TotalGas += val;
+        // }
+
+        // for (int val : cost) {
+        //     TotalCost += val;
+        // }
+
+        // if (TotalGas < TotalCost) {
+        //     return -1;
+        // }
+
+        // // unique solution-
+
+        // int start = 0, currGas = 0;
+
+        // for (int i = 0; i < gas.size(); i++) {
+        //     currGas += (gas[i] - cost[i]);
+
+        //     if (currGas < 0) {
+        //         start = i + 1;
+        //         currGas = 0;
+        //     }
+        // }
+
+        // return start;
+
+
+
+        // optimisation-
         int TotalGas = 0;
         int TotalCost = 0;
-
-        for (int val : gas) {
-            TotalGas += val;
-        }
-
-        for (int val : cost) {
-            TotalCost += val;
-        }
-
-        if (TotalGas < TotalCost) {
-            return -1;
-        }
 
         // unique solution-
 
         int start = 0, currGas = 0;
 
         for (int i = 0; i < gas.size(); i++) {
+            TotalGas += gas[i];
+            TotalCost += cost[i];
             currGas += (gas[i] - cost[i]);
 
             if (currGas < 0) {
@@ -29,6 +52,6 @@ public:
             }
         }
 
-        return start;
+        return TotalGas < TotalCost ? -1 : start;
     }
 };
