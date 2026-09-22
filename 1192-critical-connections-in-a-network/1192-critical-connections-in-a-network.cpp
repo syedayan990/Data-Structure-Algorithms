87550760ba6f21bb6829040b9dcece37bd1011d2
Syed Ayan Husain <ayanhusain903@gmail.com>
2026-09-23 04:47:49 +0530

    Time: 127 ms (98.19%), Space: 168.7 MB (92.44%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -6,7 +6,7 @@ public:
 
     void dfs(int u, int partU, vector<vector<int>>& adj,
              vector<vector<int>>& criticalConnection) {
-       
+
         dt[u] = low[u] = ++time;
 
         for (int i = 0; i < adj[u].size(); i++) {
@@ -19,10 +19,10 @@ public:
 
                 // Bridge
                 if (low[v] > dt[u]) {
-                     criticalConnection.push_back({u , v});
+                    criticalConnection.push_back({u, v});
                 }
 
-            } else if (v != partU){
+            } else if (v != partU) {
                 low[u] = min(low[u], dt[v]);
             }
         }
@@ -38,14 +38,14 @@ public:
             adj[v].push_back(u);
         }
         time = 0;
-        dt.resize(n , -1);
+        dt.resize(n, -1);
         low.resize(n);
 
         vector<vector<int>> bridges;
 
-        for(int i=0; i<n; i++){
-            if(dt[i] == -1){
-                dfs(i, -1 , adj , bridges);
+        for (int i = 0; i < n; i++) {
+            if (dt[i] == -1) {
+                dfs(i, -1, adj, bridges);
             }
         }
         return bridges;
